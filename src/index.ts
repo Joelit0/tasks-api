1e2defe8d37243a97d43adbd890823c091f017b1
PVmendez <valentino.mendez.rey@gmail.com>
2023-06-07 19:23:06 -0300

    fix: separate data dummy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Task } from './interfaces/task'
 import { User } from './interfaces/user'
+import users from '../userData';
 
 import express from 'express';
 import bodyParser from 'body-parser';
@@ -12,22 +13,6 @@ const tasksUrl = '/users/:id/tasks';
 const taskUrl = '/users/:user_id/tasks/:task_id';
 const usersUrl = '/users';
 const userUrl = '/users/:id';
-
-const users: User[] = [
-  {
-    id: '1',
-    username: 'joel',
-    password: '1234',
-    tasks: [
-      {
-        id: '1',
-        title: 'example title',
-        description: 'example description',
-        completed: false
-      }
-    ]
-  }
-];
 
 app.use(cors());
 
